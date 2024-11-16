937ccc5e4ff9142c2ad751c7e9aabcf5bae74ef4
Anasashfaq40 <anasashfaq40@gmail.com>
2024-11-16 09:52:56 -0800

    another commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutterofflie/SignUpScreen.dart';
 import 'package:flutterofflie/WatchEaseScreen.dart';
+import 'package:flutterofflie/dashboard/DashboardScreen.dart';
 
 void main() {
   runApp(WatchEaseApp());
@@ -10,7 +11,7 @@ class WatchEaseApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SignUpScreen(),
+      home: DashboardScreen(),
       theme: ThemeData(
         fontFamily: 'Nunito',
         primarySwatch: Colors.blue,
